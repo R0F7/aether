4f5749ffe32ec7fb571d486623bb85c0a2b2f4e7
R0F7 <wwwrafikhan075@gmail.com>
2026-05-07 17:03:34 +0600

    debug Vercel rendering for hero carousel and featured products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,46 @@
-import Newsletter from "@/components/newsletter";
-import BrandStatement from "@/components/brand-statement";
-import { Suspense } from "react";
-import { getCollections } from "@/lib/data";
-import { FeaturedProductsWrapper } from "@/components/featured-products-wrapper";
-import { HeroCarouselWrapper } from "@/components/hero-carousel-wrapper";
-import { HeroSkeleton } from "@/components/hero-skeleton";
+// import Newsletter from "@/components/newsletter";
+// import BrandStatement from "@/components/brand-statement";
+// import { Suspense } from "react";
+// import { getCollections } from "@/lib/data";
+// import { FeaturedProductsWrapper } from "@/components/featured-products-wrapper";
+// import { HeroCarouselWrapper } from "@/components/hero-carousel-wrapper";
+// import { HeroSkeleton } from "@/components/hero-skeleton";
 
-export default function Home() {
-  const collectionsPromise = getCollections();
+import { FeaturedProducts } from "@/components/featured-products";
+import { HeroCarousel } from "@/components/hero-carousel";
+import { getCollections, getProducts } from "@/lib/data";
 
-  // re-setup vercel env
+// export default function Home() {
+//   const collectionsPromise = getCollections();
+
+//   // re-setup vercel env
+//   return (
+//     <>
+//       {/* Hero Carousel */}
+//       <Suspense fallback={<HeroSkeleton></HeroSkeleton>}>
+//         <HeroCarouselWrapper promise={collectionsPromise} />
+//       </Suspense>
+
+//       {/* Featured Products */}
+//       <FeaturedProductsWrapper></FeaturedProductsWrapper>
+
+//       {/* Brand Statement Section */}
+//       <BrandStatement></BrandStatement>
+
+//       {/* Newsletter */}
+//       <Newsletter></Newsletter>
+//     </>
+//   );
+// }
+
+export default async function Home() {
+  const [collections] = await Promise.all([
+    getCollections(),
+  ]);
+
   return (
     <>
-      {/* Hero Carousel */}
-      <Suspense fallback={<HeroSkeleton></HeroSkeleton>}>
-        <HeroCarouselWrapper promise={collectionsPromise} />
-      </Suspense>
-
-      {/* Featured Products */}
-      <FeaturedProductsWrapper></FeaturedProductsWrapper>
-
-      {/* Brand Statement Section */}
-      <BrandStatement></BrandStatement>
-
-      {/* Newsletter */}
-      <Newsletter></Newsletter>
+      <HeroCarousel collections={collections} />
     </>
   );
 }
